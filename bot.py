19acfec580f4690680f8a948e30aec790964cfb0
shadow13579-spec <dramanekara227@gmail.com>
2026-07-22 06:56:05 +0000

    Fix final Flask Thread

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ def home():
 def run():
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
-
-# Lance le serveur web en tâche de fond
-Thread(target=run).start()
+# Lance le serveur web en tâche de fond pour Render
+t = Thread(target=run)
+t.start()
 import re
 import asyncio
 from highrise import BaseBot, User, Position, AnchorPosition
